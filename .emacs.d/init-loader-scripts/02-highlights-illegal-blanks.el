6edfeee50c9a2a97acf347a4f290a9f234c08b95
Shotaro Kohama <kohama@fout.jp>
2015-09-07 10:30:42 +0900

    Add a config for highlighting illegal blanks

---
+++ .emacs.d/init-loader-scripts/02-highlights-illegal-blanks.el
@@ -0,0 +1,16 @@
+;; highlights tailing/heading spaces and hard tab
+(defface my-face-b-1 '((t (:background "bisque"))) nil)
+(defface my-face-b-2 '((t (:background "LemonChiffon2"))) nil)
+(defface my-face-u-1 '((t (:foreground "SteelBlue" :underline t))) nil)
+(defvar my-face-b-1 'my-face-b-1)
+(defvar my-face-b-2 'my-face-b-2)
+(defvar my-face-u-1 'my-face-u-1)
+(defadvice font-lock-mode (before my-font-lock-mode ())
+  (font-lock-add-keywords
+   major-mode
+   '(("　" 0 my-face-b-1 append)
+     ("\t" 0 my-face-b-2 append)
+     ("[ \t]+$" 0 my-face-u-1 append)
+     )))
+(ad-enable-advice 'font-lock-mode 'before 'my-font-lock-mode)
+(ad-activate 'font-lock-mode)
