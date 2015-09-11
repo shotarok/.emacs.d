62e4f6324588a85e595f5f7b28c55f8cffca0891
Shotaro Kohama <kohama@fout.jp>
2015-09-11 16:13:48 +0900

    Update global keys

--- .emacs.d/init-loader-scripts/99-globl-keys.el
+++ .emacs.d/init-loader-scripts/99-globl-keys.el
@@ -1,2 +1,11 @@
+; back space
 (global-set-key "\C-h" 'delete-backward-char)
-(global-set-key "\C-c ;" 'iedit-mode)
+
+; iedit-mode
+(global-set-key (kbd "C-c ;") 'iedit-mode)
+
+;; window move
+(global-set-key (kbd "C-x <up>") 'windmove-up)
+(global-set-key (kbd "C-x <down>") 'windmove-down)
+(global-set-key (kbd "C-x <right>") 'windmove-right)
+(global-set-key (kbd "C-x <left>") 'windmove-left)
